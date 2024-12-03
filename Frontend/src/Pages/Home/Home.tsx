a26692f21f1f270e6a4428ebd191287906e7a4b4
Dima Shamoev <dimashamoev5@gmail.com>
2024-12-03 12:17:26 +0400

    restart project from client side

--- Frontend/src/Pages/Home/Home.tsx
+++ Frontend/src/Pages/Home/Home.tsx
@@ -1,31 +1,15 @@
-import React from "react";
-import "./Home.scss";
-import Header from "../../components/Header";
-import Footer from "../../components/Footer";
+import React from 'react'
+import './Home.scss'
+import './HomeResponsive.css'
 
 const Home: React.FunctionComponent = () => {
-    return (
-        <main className="home-main">
-            <Header />
-            <div className="small-container">
-                <div className="main-banner-block">
-                    <div className="main-banner-text">
-                        <div className="banner-first-text">
-                            Write Your Story, Inspire the World
-                        </div>
-                        <div className="banner-secondary-text">
-                            Where Ideas Take Flight and Words Connect
-                        </div>
-                    </div>
+  return (
+    <div className='Home'>
+        <div className="container">
+            
+        </div>
+    </div>
+  )
+}
 
-                    <div className="main-banner-img">
-                        <img src="/img/home-banner-img.png" alt="" />
-                    </div>
-                </div>
-            </div>
-            <Footer />
-        </main>
-    );
-};
-
-export default Home;
+export default Home
