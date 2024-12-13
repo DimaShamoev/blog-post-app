166ff51880dfa2ad3e090e7553375becf79ace58
Dima Shamoev <dimashamoev5@gmail.com>
2024-12-13 23:16:47 +0400

    Refreshed Deploy version

--- Frontend/src/Pages/CreateBlog/CreateBlog.tsx
+++ Frontend/src/Pages/CreateBlog/CreateBlog.tsx
@@ -25,7 +25,7 @@ const CreateBlog: React.FunctionComponent = () => {
 
     const onSubmit = async (data: IFormData) => {
         try {
-            const response = await axios.post("http://localhost:3000/blog", {
+            await axios.post("https://spaceblog-a6he.onrender.com/blog", {
                 title: data.title,
                 content: data.content,
                 blogType: data.blogType,
