ea93d6b6893d891a21382589ab68eb8ed47ff1c8
Dima Shamoev <dimashamoev5@gmail.com>
2024-12-19 18:38:58 +0400

    created debounce hook, prepared seach component

---
+++ Frontend/src/components/BlogSearch.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const BlogSearch: React.FunctionComponent = () => {
+    return (
+        <div>
+
+            {/* implement search idea */}
+
+        </div>
+    )
+}
+
+export default BlogSearch
